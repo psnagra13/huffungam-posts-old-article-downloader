48399826ad9941a7fa3d6622520bdd332e2f4f35
pardeep <psnagra13@gmail.com>
2020-05-12 06:19:38 +0530

    fixed

--- 2.GetDownloadedDictionary.py
+++ 2.GetDownloadedDictionary.py
@@ -2,6 +2,9 @@ import os
 from Utilities.UtilFunctions import *
 from Utilities.Logger import LoggerClass
 import pickle
+
+
+
 data_path = 'data/processedData'
 downloaded_files =[]
 
@@ -35,15 +38,5 @@ print(len(dd))
 
 
 
-# i=0
-# for file in downloaded_files:
-#     print(file)
-#     f = open(file, "r")
-#     raw_data = f.read().split('\n')
-#     raw_data = raw_data[:-1]
-#     raw_data = '\n'.join(raw_data)
-#     file = open('data/processedData/'+str(i), 'w')
-#     file.write(raw_data)
-#     file.close()
-#     i+=1
+
 
